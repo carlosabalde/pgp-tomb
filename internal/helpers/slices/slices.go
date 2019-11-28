4aeaade2a6d2a6b02e1c98e5218bdd38bd05b189
David Fuentes <davidfb@allenta.com>
2019-11-28 15:06:21 +0100

    Simplify helper/slices

--- internal/helpers/slices/slices.go
+++ internal/helpers/slices/slices.go
@@ -21,12 +21,13 @@ func Union(arr1, arr2 interface{}) (reflect.Value, error) {
 	// Put values of both slices as keys into a map to avoid repetitions.
 	items := make(map[interface{}]bool)
 	for _, arr := range [2]interface{}{arr1, arr2} {
-		for item := range mapFromSlice(arr) {
-			items[item] = true
+		slice := reflect.ValueOf(arr)
+		for i, n := 0, slice.Len(); i < n; i++ {
+			items[slice.Index(i).Interface()] = true
 		}
 	}
 
-	// Turn the map keys into a slice and return it.
+	// Turn map keys into a slice and return it.
 	return sliceFromMap(items, arr1), nil
 }
 
@@ -44,43 +45,26 @@ func Difference(arr1, arr2 interface{}) (reflect.Value, error) {
 
 	// Put values of the first slice as keys into a map and
 	// then remove values of the second slice.
-	items := mapFromSlice(arr1)
-	for item := range mapFromSlice(arr2) {
-		delete(items, item)
-	}
-
-	// Turn the map keys into a slice and return it.
-	return sliceFromMap(items, arr1), nil
-}
-
-func Distinct(arr interface{}) (reflect.Value, error) {
-	// Make sure input is a slice.
-	if reflect.TypeOf(arr).Kind() != reflect.Slice {
-		return reflect.Value{}, errors.New("not a slice")
-	}
-
-	// Put values of the slice as keys into a map to avoid repetitions
-	// and then turn the map keys into a slice again and return it.
-	return sliceFromMap(mapFromSlice(arr), arr), nil
-}
-
-func mapFromSlice(arr interface{}) map[interface{}]bool {
 	items := make(map[interface{}]bool)
-	slice := reflect.ValueOf(arr)
-	for i, n := 0, slice.Len(); i < n; i++ {
-		items[slice.Index(i).Interface()] = true
+	slice1 := reflect.ValueOf(arr1)
+	for i, n := 0, slice1.Len(); i < n; i++ {
+		items[slice1.Index(i).Interface()] = true
 	}
-	return items
+	slice2 := reflect.ValueOf(arr2)
+	for i, n := 0, slice2.Len(); i < n; i++ {
+		delete(items, slice2.Index(i).Interface())
+	}
+
+	// Turn map keys into a slice and return it.
+	return sliceFromMap(items, arr1), nil
 }
 
 func sliceFromMap(items map[interface{}]bool, arr interface{}) reflect.Value {
 	result := reflect.MakeSlice(reflect.TypeOf(arr), len(items), len(items))
-
 	i := 0
 	for item := range items {
 		result.Index(i).Set(reflect.ValueOf(item))
 		i++
 	}
-
 	return result
 }
